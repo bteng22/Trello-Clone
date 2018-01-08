f44d7b6227560cbc6ee82ec0139eb984e5952229
bteng22 <bteng22@gmail.com>
2018-01-08 12:30:19 -0800

    moving components into directory

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Boards from './Boards.js';
-import './App.css';
 
 const StyledAppContainer = styled.div``
 
