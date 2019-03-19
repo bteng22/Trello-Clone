3b6dfc165f9d232f172c87fa5cfc15a32b67bb4f
bteng22 <bteng22@gmail.com>
2019-03-19 11:30:27 -0700

    Revert "fetching boardpage from localStorage"
    This reverts commit 89e1026cfae0a2528d4915ad353c922afd122b84.

--- src/components/BoardPage.js
+++ src/components/BoardPage.js
@@ -1,45 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 import Header from './Header';
-import storage from '../storage';
 
 const StyledBoardPageContainer = styled.div`
 `
 
 class BoardPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: ''
-    }
-    this.fetchBoardFromLocalStorage = this.fetchBoardFromLocalStorage.bind(this);
-  }
-
-  fetchBoardFromLocalStorage() {
-    const boards = storage(localStorage).get('boards');
-    if (boards.length) {
-      const board = boards.find(({ id }) => id === this.props.id);
-      this.setState({
-        title: board.title
-      });
-    }
-  }
-
-  componentDidMount() {
-    this.fetchBoardFromLocalStorage()
-  }
-
   render() {
     return (
       <Fragment>
         <Header />
         <StyledBoardPageContainer>
-          <div>
-            <h1>{this.state.title}</h1>
-          </div>
-          <div>
-            BOARD PAGE DETAILS
-          </div>
+          BOARD PAGE
         </StyledBoardPageContainer>
       </Fragment>
     )
